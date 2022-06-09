fe2a4562390d4bdb9a1c182426ca10d48cb45923
Bradley Burns <bradley.burns@codethink.co.uk>
2022-06-09 08:49:09 +0100

    Add initial set of links for the terminal config

--- scripts/link.sh
+++ scripts/link.sh
@@ -2,3 +2,6 @@
 
 ## Creates static links between the dotfiles in this repo to their respective
 ## configuration locations.
+
+ln -sf terminal/starship.conf ~/.config/starship.conf
+ls -sf terminal/.zshrc ~/.zshrc
