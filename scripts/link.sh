95c1b1f2b2b62a6b023cf802d9f7ec1fdd2299d5
Bradley Burns <bradley.burns@codethink.co.uk>
2022-06-09 10:47:28 +0100

    Fix wrong filename for Starhsip
    Original link.sh had `starhsip.conf`, this is incorrect, and should be
    changed to `starship.toml`.

--- scripts/link.sh
+++ scripts/link.sh
@@ -3,5 +3,5 @@
 ## Creates static links between the dotfiles in this repo to their respective
 ## configuration locations.
 
-ln -sf terminal/starship.conf ~/.config/starship.conf
+ln -sf terminal/starship.toml ~/.config/starship.toml
 ls -sf terminal/.zshrc ~/.zshrc
