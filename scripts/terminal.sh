8bc78c61963d9687a7c1ddce0d05aedd6fbc2f36
Bradley Burns <bradleyburns00@gmail.com>
2022-06-15 21:29:44 +0100

    Add command to install nano syntax highlighting

--- scripts/terminal.sh
+++ scripts/terminal.sh
@@ -13,5 +13,8 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
 
+# Install Nano Syntax highlighting
+curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
+
 # After these have been installed, the script `link.sh` will deal with the
 # config files
