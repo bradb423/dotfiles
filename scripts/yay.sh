9ddc502b947c7d3c3b31667533af5eef822c7bc1
Bradley Burns <bradleyburns00@gmail.com>
2022-06-15 20:43:33 +0100

    Add script to install yay and AUR packages

--- scripts/yay.sh
+++ scripts/yay.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-## A script to install the AUR helper, YAY.
+## A script to install the AUR helper, [yay](https://github.com/Jguer/yay).
+## Also use YAY to install my usual AUR packages
 
 git clone https://aur.archlinux.org/yay.git
 cd yay || exit
 makepkg -si
 yay --version
+
+yay -S \
+    google-chrome \
+    nerd-fonts-complete-starship \
+    spotify \
+    visual-studio-code-bin
