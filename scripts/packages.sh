1ced725e33e12d7391aaa0e45588e3e28a68b1a0
Bradley Burns <bradleyburns00@gmail.com>
2022-06-15 20:42:22 +0100

    Add script to install packages via pacman

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
 
-## A script to install the necessary packages, both from
-## [pacman](https://wiki.archlinux.org/title/pacman) and the
-## [AUR](https://wiki.archlinux.org/title/Arch_User_Repository) via
-## [yay](https://github.com/Jguer/yay)
+## A script to install the necessary packages.
+
+sudo pacman -S \
+    base-devel \
+    zsh \
+    starship \
+    git \
+    gnome-boxes \
+    neofetch htop \
+    discord \
+    xorg \
+    xorg-server \
+    gnome \
+    gnome-tweaks \
+    firefox \
+    bluez \
+    bluez-libs \
+    bluez-utils \
+    curl \
+    pipewire \
+    nano \
+
+# Start xorg and the display manager GDM
+sudo systemctl enable gdm.service
+sudo systemctl enable NetworkManager
